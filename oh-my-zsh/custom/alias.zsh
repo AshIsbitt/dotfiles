fc5ff76cafacd1efcc8e93fe084375ec76023ebb
Ttibsi <ashisbitt@icloud.com>
2022-02-22 11:37:36 +0000

    Update la alias with bytes displayed on files

--- oh-my-zsh/custom/alias.zsh
+++ oh-my-zsh/custom/alias.zsh
@@ -1,5 +1,5 @@
 alias gitgraph="git log --oneline --graph --decorate --all"
-alias la="ll -A"
+alias la="ls -Alh"
 alias ghb="gh browse"
 alias gbl="git branch --list"
 alias ghstat="python3 -m gh_stats.ghstat"
