5c71e48f62d1a097022f04efe8ca762494a3468d
Ash <ashisbitt@icloud.com>
2022-09-16 12:07:30 +0100

    SSH and gsettings

--- dotfiles.sh
+++ dotfiles.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
+SSH_DIR="$HOME/.ssh"
+
 # apt update && upgrade
 sudo apt update && sudo apt upgrade -y
 
 # Remove old installs/files
 
 # gnome settings
+echo -e "\nShow Percentage in top bar"
+gsettings set org.gnome.desktop.interface show-battery-percentage true
+
 
 # apt install everything
 
@@ -16,3 +21,14 @@ sudo apt update && sudo apt upgrade -y
 # set up symlinks
 
 # Generate SSH key
+if ! [[ -f "$SSH_DIR/authorized_keys" ]]; then
+    echo -e "\n----- Generating SSH key -----"
+    mkdir -p "$SSH_DIR"
+    chmod 700 "$SSH_DIR"
+    ssh-keygen -b 4096 -t rsa -f "$SSH_DIR/id_rsa" -N "" -C "$USER@$HOSTNAME"
+    cat "$SSH_DIR/id_rsa.pub" >> "$SSH_DIR/authorized_keys"
+fi
+
+echo -e "\n----- Copying SHH pub key to clipboard -----"
+cat "$SSH_DIR/id_rsa.pub" | xclip -selection c
+echo Add to account here: https://github.com/settings/keys
