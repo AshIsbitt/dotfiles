4e49b98d893b04a18e2b892ff261039e836431a9
Ash <ashisbitt@icloud.com>
2022-09-16 10:48:10 +0100

    Shell executable

--- dotfiles.sh
+++ dotfiles.sh
@@ -1,4 +1,9 @@
+#!/bin/bash
+
 # apt update && upgrade
+sudo apt update && sudo apt upgrade -y
+
+# Remove old installs/files
 
 # gnome settings
 
@@ -6,4 +11,8 @@
 
 # manual install - neovim
 
+# File structure setup
+
 # set up symlinks
+
+# Generate SSH key
