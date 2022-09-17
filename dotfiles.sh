4c9b7c857a4f63ffb02a406d19e6d4f46893a1a0
Ttibsi <ashisbitt@icloud.com>
2022-09-17 12:00:00 +0100

    Add headers to each section

--- dotfiles.sh
+++ dotfiles.sh
@@ -3,32 +3,38 @@
 SSH_DIR="$HOME/.ssh"
 
 # apt update && upgrade
+echo -e "-----UPDATE && UPGRADE-----"
 sudo apt update && sudo apt upgrade -y
 
 # Remove old installs/files
+echo -e "-----REMOVE UNNEEDED FILES-----"
 
 # gnome settings
-echo -e "\nShow Percentage in top bar"
+echo -e "-----GNOME SETTINGS-----"
 gsettings set org.gnome.desktop.interface show-battery-percentage true
 
 
 # apt install everything
+echo -e "-----APT INSTALL-----"
 
 # manual install - neovim
 
 # File structure setup
+echo -e "-----SET UP FILE STRUCTURE-----"
 
 # set up symlinks
+echo -e "-----SET UP CONFIG SYMLINKS-----"
 
 # Generate SSH key
+echo -e "-----GENERATE SSH KEY-----"
 if ! [[ -f "$SSH_DIR/authorized_keys" ]]; then
-    echo -e "\n----- Generating SSH key -----"
     mkdir -p "$SSH_DIR"
     chmod 700 "$SSH_DIR"
     ssh-keygen -b 4096 -t rsa -f "$SSH_DIR/id_rsa" -N "" -C "$USER@$HOSTNAME"
     cat "$SSH_DIR/id_rsa.pub" >> "$SSH_DIR/authorized_keys"
 fi
 
-echo -e "\n----- Copying SHH pub key to clipboard -----"
 cat "$SSH_DIR/id_rsa.pub" | xclip -selection c
 echo Add to account here: https://github.com/settings/keys
+
+echo -e "Configuration complete"
