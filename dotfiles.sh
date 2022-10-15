fc77c95f93a37ab4686b53a3ed9347b899d92552
ttibsi <ashisbitt@icloud.com>
2022-10-15 14:26:56 +0100

    fix install issues

--- dotfiles.sh
+++ dotfiles.sh
@@ -8,11 +8,12 @@ sudo apt update && sudo apt upgrade -y
 
 # Remove old installs/files
 echo -e "-----REMOVE UNNEEDED FILES-----"
-sudo rm -rf /usr/local/go
 sudo rm -rf $HOME/Workspace \
     $HOME/.opt \
-    $HOME/.opt/lua-language-server \
-    /usr/lib/firefox/distribution/policies.json \
+    /usr/local/go \
+    /usr/local/bin/nvim \
+    /usr/bin/btm \
+
 
 # gnome settings
 echo -e "-----GNOME SETTINGS-----"
@@ -77,13 +78,14 @@ l$HOME/.opt/venv/bin/python3 -m pip install --upgrade pip
 echo -e "-----MANUAL INSTALLATIONS-----"
 # manual install - go, fonts, neovim, OMZ
 curl -LO https://go.dev/dl/go1.19.2.linux-amd64.tar.gz
-sudo tar -C /usr/local -xzf go1.19.2.linux-amd64.tar.gz
+sudo tar -C /usr/local -xzf $HOME/Downloads/go1.19.2.linux-amd64.tar.gz
 
 curl -L https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/DroidSansMono/complete/Droid%20Sans%20Mono%20Nerd%20Font%20Complete%20Mono.otf > DroidSansMono_NerdFont
 mv $PWD/DroidSansMono_NerdFont $HOME/.local/share/fonts/
 
-git clone https://github.com/neovim/neovim.git 
-cd neovim
+git clone https://github.com/neovim/neovim.git $HOME/Downloads/neovim 
+cd $HOME/Downloads/neovim
+git checkout stable
 make CMAKE_BUILD_TYPE=RelWithDebInfo
 sudo make install
 cd ..
@@ -94,10 +96,10 @@ git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$
 
 git clone https://github.com/savq/paq-nvim.git $HOME/.local/share/nvim/site/pack/paqs/start/paq-nvim/
 
-$HOME/opt/venv/bin/pip install python-lsp-server
+$HOME/.opt/venv/bin/pip install python-lsp-server
 
-curl -Lo https://github.com/sumneko/lua-language-server/releases/download/3.5.6/lua-language-server-3.5.6-linux-x64.tar.gz $HOME/Downloads/lls.tar.gz
-sudo tar -C $HOME./opt/lua-language-server -xzf $HOME/Downloads/lls
+curl -Lo  $HOME/Downloads/lls.tar.gz https://github.com/sumneko/lua-language-server/releases/download/3.5.6/lua-language-server-3.5.6-linux-x64.tar.gz
+sudo tar -C $HOME/.opt/lua-language-server -xzf $HOME/Downloads/lls.tar.gz
 
 go install golang.org/x/tools/gopls@latest
 
@@ -105,11 +107,12 @@ nvim --headless -c 'PaqInstall' +q
 
 # set up symlinks
 echo -e "-----SET UP CONFIG SYMLINKS-----"
+sudo rm /usr/lib/firefox/distribution/policies.json 
 git clone https://github.com/Ttibsi/dotfiles.git $HOME/Workspace/dotfiles
 
 ln -s $HOME/Workspace/dotfiles/configs/firefox/policies.json /usr/lib/firefox/distribution/policies.json
 ln -s $HOME/Workspace/dotfiles/configs/git-config/gitconfig $HOME/.gitconfig
-ln -s $HOME/Workspace/dotfiles/configs/internal-configs/90-touchpad.conf /etc/X11/xorg.conf.d/90-touchpad.conf
+sudo ln -s $HOME/Workspace/dotfiles/configs/internal-configs/90-touchpad.conf /etc/X11/xorg.conf.d/90-touchpad.conf
 ln -s $HOME/Workspace/dotfiles/configs/nvim $HOME/.config/nvim
 ln -s $HOME/Workspace/dotfiles/configs/tmux $HOME/.config/tmux
 ln -s $HOME/Workspace/dotfiles/configs/zsh-shell/ $HOME/
