f5116b815f503570c9efeedf7c9a28642cf53a00
ttibsi <ashisbitt@icloud.com>
2022-10-15 14:20:40 +0100

    Hopefully stop omz from getting in the way

--- dotfiles.sh
+++ dotfiles.sh
@@ -89,7 +89,7 @@ sudo make install
 cd ..
 rm -rf neovim
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+CHSH="no" sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh --unattended --skip-chsh)"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
 git clone https://github.com/savq/paq-nvim.git $HOME/.local/share/nvim/site/pack/paqs/start/paq-nvim/
